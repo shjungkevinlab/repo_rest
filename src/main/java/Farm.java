e69125e01e12843573a9d48753e68ffd07b8f6b9
user <user@DESKTOP-I9KJHMU>
2023-11-10 14:58:02 +0900

    문자열 교체 #2 fix : catch > title

--- src/main/java/Farm.java
+++ src/main/java/Farm.java
@@ -3,7 +3,7 @@ public class Farm {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("good catch ~~ !!");
+		System.out.println("good title ~~ !!");
 		System.out.println("welcome");
 		System.out.println("hellow");
 	}
